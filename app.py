fedf9254ea319a3b320440d6973f045fe1be5ae3
andrenguyening <35712233+andrenguyening@users.noreply.github.com>
2024-02-26 19:41:36 -0500

    Created app.py, created main pages

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return render_template('layout.html')
+
+@app.route("/market")
+def market():
+    return render_template('market.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
